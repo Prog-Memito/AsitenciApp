1129c02e119c07ceae4401d7f5feabcad6d147a9
Prog-Memito <sa3776530@gmail.com>
2024-11-17 14:15:26 -0300

    Login con FireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+
+    apiKey: "AIzaSyDnuy9ON9ypAUQpqcsjiZr9zpWElT3jA7k",
+  
+    authDomain: "asistenciapp-4c57b.firebaseapp.com",
+  
+    projectId: "asistenciapp-4c57b",
+  
+    storageBucket: "asistenciapp-4c57b.firebasestorage.app",
+  
+    messagingSenderId: "275305031470",
+  
+    appId: "1:275305031470:web:471ee76459431247dc3cbf"
+  
+  };
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+
+    apiKey: "AIzaSyDnuy9ON9ypAUQpqcsjiZr9zpWElT3jA7k",
+  
+    authDomain: "asistenciapp-4c57b.firebaseapp.com",
+  
+    projectId: "asistenciapp-4c57b",
+  
+    storageBucket: "asistenciapp-4c57b.firebasestorage.app",
+  
+    messagingSenderId: "275305031470",
+  
+    appId: "1:275305031470:web:471ee76459431247dc3cbf"
+  
+  };
+  
 };
 
 /*
