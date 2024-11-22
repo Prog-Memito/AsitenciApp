839376c3ac9e1ef3ea7470a15aa4fe51e73d8e4e
Prog-Memito <sa3776530@gmail.com>
2024-11-22 12:33:36 -0300

    Funcion singOut creada

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, up
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore'
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class FirebaseService {
 
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  utilSvc = inject(UtilsService);
 
   /* Autenticacion */
 
@@ -22,6 +24,13 @@ export class FirebaseService {
   /* Acceder */
   singIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  /* Cerrar sesión */
+  singOut() {
+    getAuth().signOut();
+    localStorage.removeItem('user');
+    this.utilSvc.routerLink('/auth');
   }
 
   /* Registro */
