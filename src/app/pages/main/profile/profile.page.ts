5ee13ba14c4b216944b65dd2cdce9d77d22aa6d3
Prog-Memito <sa3776530@gmail.com>
2024-11-24 02:33:33 -0300

    Perfil creado

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  FirebaseSvc = inject(FirebaseService);
+  utilsSvs = inject(UtilsService);
 
   ngOnInit() {
+  }
+
+  user(): User {
+    return this.utilsSvs.getFromLocalStorage('user');
+  }
+
+  async takeImage() {
+
+    let user = this.user();
+    let path = `users/${user.uid}`
+
+    const dataUrl = (await this.utilsSvs.takePicture('Imagen del perfil')).dataUrl;
+
+    const loading = await this.utilsSvs.loading();
+    await loading.present();
+
+    let imagePath = `${user.uid}/profile`;
+    user.image = await this.FirebaseSvc.uploadImage(imagePath, dataUrl);
+
+    this.FirebaseSvc.updateDocument(path, {image: user.image}).then(async res => {
+
+      this.utilsSvs.saveInLocalStorage('user', user);
+
+      this.utilsSvs.presentToast({
+        message: 'Imagen acutalizada correctamente',
+        duration: 1500,
+        color: 'success',
+        position: 'middle',
+        icon: 'checkmark-circle-outline'
+      })
+
+
+    }).catch(error => {
+      console.log(error);
+
+      this.utilsSvs.presentToast({
+        message: error.message,
+        duration: 2500,
+        color: 'primary',
+        position: 'middle',
+        icon: 'alert-circle-outline'
+      })
+
+    }).finally(() => {
+      loading.dismiss();
+    })
   }
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -3,8 +3,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail } from 'firebase/auth'
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore'
+import { getFirestore, setDoc, doc, getDoc, updateDoc } from '@angular/fire/firestore'
 import { UtilsService } from './utils.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { getStorage, uploadString, ref, getDownloadURL } from 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,7 @@ export class FirebaseService {
 
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  storage = inject(AngularFireStorage);
   utilSvc = inject(UtilsService);
 
   /* Autenticacion */
@@ -55,8 +58,20 @@ export class FirebaseService {
     return setDoc(doc(getFirestore(), path), data);
   }
 
+  /* Actualizar un documento */
+  updateDocument(path: string, data: any) {
+    return updateDoc(doc(getFirestore(), path), data);
+  }
+
   /* Obtener un documento */
   async getDocument(path: string) {
     return (await getDoc(doc(getFirestore(), path))).data();
+  }
+
+  /* Almacenamiento */
+  async uploadImage(path: string, data_url: string) {
+    return uploadString(ref(getStorage(), path), data_url, 'data_url').then(() => {
+      return getDownloadURL(ref(getStorage(), path))
+    })
   }
 }
