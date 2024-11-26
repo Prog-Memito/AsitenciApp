d30b234dbeac7f9886e59def12294b0d0d8ffad3
Prog-Memito <sa3776530@gmail.com>
2024-11-26 18:03:27 -0300

    Pagina asistencia creada

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -16,7 +16,11 @@ const routes: Routes = [
         path: 'profile',
         loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
       },]
+  },  {
+    path: 'asistencia',
+    loadChildren: () => import('./asistencia/asistencia.module').then( m => m.AsistenciaPageModule)
   },
+
 
 
 ];
